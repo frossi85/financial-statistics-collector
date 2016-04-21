0c61af5d4158a3524223d403860ab40815f8570b
Facundo Rossi <frossi85@gmail.com>
2016-04-21 18:38:23 -0300

    Some minor changes

--- src/main/scala/com/example/DatabaseDumpActor.scala
+++ src/main/scala/com/example/DatabaseDumpActor.scala
@@ -1,11 +1,20 @@
 package com.example
 
 import akka.actor.{Actor, ActorLogging, Props}
+import com.typesafe.scalalogging.LazyLogging
 
-class DatabaseDumpActor extends Actor with ActorLogging with AutoMarshaller {
+class DatabaseDumpActor extends Actor with ActorLogging with AutoMarshaller with LazyLogging {
   def receive = {
   	case statistic: StatisticData =>
-      new MongoDBHelper().statisticsCollection.insert(statistic.toMongoDBObject)
+      try {
+        new MongoDBHelper().statisticsCollection.insert(statistic.toMongoDBObject)
+      } catch {
+        case ex =>{
+          logger.error("There was an error trying to save the statistic",ex)
+          Thread.sleep(5000)
+          self ! statistic
+        }
+      }
   }
 }
 
--- src/main/scala/com/example/MongoDBHelper.scala
+++ src/main/scala/com/example/MongoDBHelper.scala
@@ -6,8 +6,9 @@ import com.typesafe.config.ConfigFactory
 class MongoDBHelper {
   val config = ConfigFactory.load()
 
-  val mongoClient = MongoClient(config.getString("mongo.host"), config.getInt("mongo.port"))
-  val db = mongoClient(config.getString("mongo.database"))
-
-  def statisticsCollection = db(config.getString("mongo.statistics_collection"))
+  def statisticsCollection = {
+    val mongoClient = MongoClient(config.getString("mongo.host"), config.getInt("mongo.port"))
+    val db = mongoClient(config.getString("mongo.database"))
+    db(config.getString("mongo.statistics_collection"))
+  }
 }
